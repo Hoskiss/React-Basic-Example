52ec719d2f46ba486e4cc8b643027dc26efae2ab
Hoskiss <hoskiss@gmail.com>
2014-12-10 09:54:49 +0800

    item compo

--- public/javascripts/navigation.jsx
+++ public/javascripts/navigation.jsx
@@ -6,20 +6,59 @@ var ButtonGroup = ReactBootstrap.ButtonGroup;
 (function () {
     'use strict';
 
-    app.NavBar = React.createClass({
+    app.TodoList = React.createClass({
         render: function() {
-            // TODO: remove hard-code, we know there're four elems
             return (
-                <div>
-                    <ButtonGroup className="col-md-12">
-                        <Button bsStyle="primary" className="col-md-3">Todo List</Button>
-                        <Button bsStyle="primary" className="col-md-3">Services</Button>
-                        <Button bsStyle="primary" className="col-md-3">About</Button>
-                        <Button bsStyle="primary" className="col-md-3">Contact us</Button>
-                    </ButtonGroup>
+                <div className="todo-list">
+                    <input placeholder="hehe"></input>
                 </div>
             );
         }
     });
+
+    app.NavBar = React.createClass({
+
+        handleTodoClick: function() {
+            console.log("hehfal");
+            var todoList = app.TodoList;
+            React.renderComponent(
+                <todoList />,
+                document.getElementsByClassName('main-content')
+            );
+        },
+
+        handleOthersClick: function() {
+            return
+        },
+
+
+
+        render: function() {
+            return (
+                <div>
+                    <div className="nav-bar">
+                        <ButtonGroup className="col-md-12">
+                            <Button bsStyle="primary" className="col-md-3" onClick={this.handleTodoClick}>
+                                Todo List
+                            </Button>
+                            <Button bsStyle="primary" className="col-md-3">Services</Button>
+                            <Button bsStyle="primary" className="col-md-3">About</Button>
+                            <Button bsStyle="primary" className="col-md-3">Contact us</Button>
+                        </ButtonGroup>
+                    </div>
+
+                    <div className="main-content">
+                    </div>
+                </div>
+            );
+        }
+    });
+
+
+
+
+
+
+
 })();
 
